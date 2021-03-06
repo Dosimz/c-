0c80774d5862b088a34e616091f17cb0627f3bb2
鱼一一 <yuyy@yuyiyideMBP.lan>
2021-03-06 16:04:21 +0800

    字符三角形标准答案

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@ int main() {
     char c;
     cout << "Please enter the charest.\n";
     cin >> c;
-    int j= 1;
-    for (int i=6; i>0; i--) {
-        cout<<string(i, ' ');
-        cout << string(j, c) + "\n";
-        j += 2;
-    }
+//    int j= 1;
+//    for (int i=6; i>0; i--) {
+//        cout<<string(i, ' ');
+//        cout << string(j, c) + "\n";
+//        j += 2;
+//    }
+//  标准答案
+    for(int i=1; i<=7; i++)
+        cout<<string(7-i, ' ')+string(2*i-1, c)+'\n';
     return 0;
 }
